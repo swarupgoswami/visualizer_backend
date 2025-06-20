bf5f7d25aef47b69a96ed89a374c705f5bcd007f
swarup Goswami <goswamiswarup97@gmail.com>
2025-06-21 01:13:09 +0530

    added the pdfparser in utils

---
+++ utils/pdfParser.js
@@ -0,0 +1,19 @@
+import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.js";
+
+export async function extractTextFromPdf(buffer) {
+  const loadingTask = pdfjsLib.getDocument({ data: buffer });
+  const pdf = await loadingTask.promise;
+  console.log(`📄 PDF loaded: ${pdf.numPages} pages`) ;
+
+  let fullText = "";
+
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const content = await page.getTextContent();
+    const pageText = content.items.map(item => item.str).join(" ");
+    fullText += pageText + "\n";
+  }
+  console.log("📄 Extracted PDF Text:", fullText.slice(0, 300)); // show first 300 chars
+  return fullText;
+}
+
