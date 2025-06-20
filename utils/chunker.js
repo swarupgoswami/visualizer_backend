4e792bbd1b63710b5d2f3bf4419245771eed840f
swarup Goswami <goswamiswarup97@gmail.com>
2025-06-21 01:13:56 +0530

    aded the chunker part to chunk the large text

---
+++ utils/chunker.js
@@ -0,0 +1,7 @@
+export function chunkText(text, maxLength = 2000) {
+  const chunks = [];
+  for (let i = 0; i < text.length; i += maxLength) {
+    chunks.push(text.slice(i, i + maxLength));
+  }
+  return chunks;
+}
